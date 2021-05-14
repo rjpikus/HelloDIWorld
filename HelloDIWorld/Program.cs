9caf44299679b97092667da2f8ac7354b4bb9ebe
rjpikus <raymondjackpikus@gmail.com>
2021-05-14 09:00:46 -0400

    first commit

---
+++ HelloDIWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloDIWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Command1;
+            Console.WriteLine("V^V online");
+            Command1 = Console.ReadLine();
+            if (Command1 == "hello")
+            {
+                Console.WriteLine("world!");
+            }
+
+            else
+            {
+                Console.WriteLine("Error: Did not type 'hello'");
+            }
+
+            //next step is to compare to HelloDI
+        }
+    }
+}
